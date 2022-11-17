3274ade67bca2c1228cee3c233215f7909e80805
Steffogaming <111148397+Steffogaming@users.noreply.github.com>
2022-11-17 11:25:01 +0100

    Initial Commit

---
+++ PiApprox/PiApprox/Fractions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Data.Common;
+
+namespace Fractions
+{
+    public struct Fraction
+    {
+        public BigInteger top;
+        public BigInteger bot;
+
+        public Fraction(BigInteger top, BigInteger bot)
+        {
+            this.top = top;
+            this.bot = bot;
+        }
+
+        public BigInteger GCD(BigInteger a, BigInteger b)
+        {
+            while(b != 0)
+            {
+                BigInteger temp = a;
+                a = b;
+                b = temp % b;
+            }
+            return a;
+        }
+
+        public void Simplify()
+        {
+            BigInteger gcd = GCD(top, bot);
+            top /= gcd;
+            bot /= gcd;
+        }
+
+        public static Fraction operator +(Fraction left, Fraction right) => new Fraction(left.top * right.bot + right.top * left.bot, left.bot * right.bot);
+        public static Fraction operator -(Fraction a) => new Fraction(-a.top, a.bot);
+        public static Fraction operator -(Fraction left, Fraction right) => left + (-right);
+        public static Fraction operator *(Fraction left, Fraction right) => new Fraction(left.top * right.top, left.bot * right.bot);
+        public static Fraction operator /(Fraction left, Fraction right) => new Fraction(left.top * right.bot, left.bot * right.top);
+
+        public static implicit operator Fraction((BigInteger, BigInteger) a) => new Fraction(a.Item1, a.Item2);
+    }
+}
---
+++ PiApprox/PiApprox/Program.cs
@@ -0,0 +1,22 @@
+﻿using Fractions;
+namespace PiApprox
+{
+    internal class Program
+    {
+        public static Fraction Approx(int n)
+        {
+            Fraction result = new Fraction(1, 1);
+            for (int i = 1; i <= n; i++)
+            {
+                result += i % 2 == 0 ? new Fraction(1, 2 * i + 1) : new Fraction(-1, 2 * i + 1);
+            }
+            return new Fraction(4, 1) * result;
+        }
+        static void Main(string[] args)
+        {
+            Fraction res = Approx(100000);
+            res.Simplify();
+            Console.WriteLine($"{res.top} / {res.bot}");
+        }
+    }
+}
